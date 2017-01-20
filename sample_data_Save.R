9b9773cf4c99b3068aea3910200aa2ee4672b883
wxm19071 <wxm19071@gmail.com>
2017-01-20 18:00:05 -0500

    Update sample_data_Save.R

--- sample_data_Save.R
+++ sample_data_Save.R
@@ -1,19 +1,19 @@
-filePathRoot <- 'C:\\Users\\deifen\\Documents\\Projects\\Bias and overfitting trade offs\\Bias and Variance Tradeoff Project\\SampleData\\dataSample'
+filePathRoot <- '/Users/wing/Downloads/Bias-Variance-Tradeoff-master/SampleData1/sampledata'
 
-n <- 750
-r <- 50
-vect <- seq(n) 
+n <- 25
+r <- 20
+vect <- seq(from= -pi, to =pi, by=.1) 
 signal<-function(n)
-{(n^2.75)/exp(sqrt(n/2)) +.40*n -sqrt(n)}
+{sin(n)}
 for(i in 1:10) 
 {
   x <- sample(vect, r)
-  noise<- rnorm(r, mean=10, sd=20)
+  noise<- rnorm(r, mean=0, sd=.1)
   y <- signal(x) + noise
   Data <- data.frame(x, y)
-  write.csv(Data, file=paste(filePathRoot, i, collapse=NULL), quote=FALSE, row.names=FALSE, append=FALSE)
+  write.csv(Data, file=paste(filePathRoot, i, collapse=NULL), quote=FALSE, row.names=FALSE)
 }
 
 plot(y ~ x, Data)
-curve(signal(x), from=1, to=n, , xlab="x", ylab="y", add=TRUE)
+curve(signal(x), from=-pi, to=pi, , xlab="x", ylab="y", add=TRUE)
 
