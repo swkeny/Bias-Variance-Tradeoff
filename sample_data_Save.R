d0690ea19a8a8354d704281956b6e696963edd18
swkeny <swk.kenyon@gmail.com>
2017-01-25 22:59:18 -0500

    Added test data set. Fixed ranges in FOR loops. Started bias calculation

--- sample_data_Save.R
+++ sample_data_Save.R
@@ -1,5 +1,5 @@
-filePathRoot <- 'C:\\Users\\deifen\\Documents\\Projects\\Bias and overfitting trade offs\\Project\\SampleData\\sampledata'
-
+filePathRootTrain <- 'C:\\Users\\deifen\\Documents\\Projects\\Bias and overfitting trade offs\\Project\\SampleData\\train'
+filePathRootTest <- 'C:\\Users\\deifen\\Documents\\Projects\\Bias and overfitting trade offs\\Project\\SampleData\\test'
 n <- 25
 r <- 20
 vect <- seq(from= -pi, to =pi, by=.1) 
@@ -10,11 +10,21 @@ for(i in 1:10)
   x <- sample(vect, r)
   noise<- rnorm(r, mean=0, sd=.5)
   y <- signal(x) + noise
-  Data <- data.frame(x, y)
-  write.csv(Data, file=paste(filePathRoot, i, collapse=NULL), quote=FALSE, row.names=FALSE)
+  trainData <- data.frame(x, y)
+  write.csv(trainData, file=paste(filePathRootTrain, i, collapse=NULL), quote=FALSE, row.names=FALSE)
 }
 
-plot(y ~ x, Data)
+x <- sample(vect, r)
+noise<- rnorm(r, mean=0, sd=.5)
+y <- signal(x) + noise
+testData <- data.frame(x, y)
+write.csv(testData, file=paste(filePathRootTest, 1, collapse=NULL), quote=FALSE, row.names=FALSE)
+
+
+plot(y ~ x, trainData)
+curve(signal(x), from=-pi, to=pi, , xlab="x", ylab="y", add=TRUE)
+
+plot(y ~ x, testData)
 curve(signal(x), from=-pi, to=pi, , xlab="x", ylab="y", add=TRUE)
 
 
