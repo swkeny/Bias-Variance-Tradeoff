c621b7ef8e41498bc2ff8e34338e7d7cc2b7a6ef
swkeny <swk.kenyon@gmail.com>
2017-01-26 23:18:04 -0500

    Data creation now in Python!

--- Python Scripts/polynomialfitTest.py
+++ Python Scripts/polynomialfitTest.py
@@ -1,109 +1,111 @@
+import math
+import random
+from sklearn import cross_validation, linear_model
+from pylab import *
+import random
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from pylab import *
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
-ymin = -math.pi
-ymax = math.pi
-polynomialDegrees = [1, 5, 9, 14, 19]
-fileRoot = "C:/Users/deifen/Documents/Projects/Bias and overfitting trade offs/Project/SampleData/sampledata "
+xmin = -math.pi
+xmax = math.pi
+ymin = -2
+ymax = 2
+polynomialDegrees = [1, 2, 3, 4, 13]
+dataPointsPerTrainingSet = 80
+#Needs numberOfTrainingSets elements in the seedMap
+seedMap = [123, 232, 13, 100, 344, 45, 71, 99, 199, 80]
+numberOfTrainingSets = seedMap.__len__()
+# generate noisy data from an underlying function
+def createSineData(n, seed):
+    random.seed(seed) # for reproducibility
+    x = pd.Series([random.uniform(-math.pi, math.pi) for i in range(n)])
+    f = np.vectorize(lambda x: math.sin(x)) # our true function
+    y = x.apply(f) # generate our labels/outputs
+    e = pd.Series([random.gauss(0,1.0/3.0) for i in range(n)]) # add some noise
+    y = y + e
+    return x, y
 
 def training_MSE_ByComplexity(dataSet, solutionSet):
       solutionBias = {}
-      for i in range(1, 10):
-            x = dataSet[i][1]
-            y = dataSet[i][2]
+      for i in range(numberOfTrainingSets):
+            x = dataSet[i]['x']
+            y = dataSet[i]['y']
             solutionBias[i] = {}
-            for s in range(1, polynomialDegrees.__len__() + 1):
+            for s in range(polynomialDegrees.__len__()):
                   solutionBias[i][s] = np.mean((np.polyval(solutionSet[i][s], x) - y) ** 2)
       return solutionBias
 
-def solutionBias_ByComplexity(dataSet, solutionSet,):
-      solutionBias = {}
-      for i in range(1, 10):
-            x = dataSet[i][1]
-            y = dataSet[i][2]
-            solutionBias[i] = {}
-            for s in range(1, polynomialDegrees.__len__() + 1):
-                  solutionBias[i][s] = np.mean(np.polyval(solutionSet[i][s], x) - y)
-      return solutionBias
-
-files = {}
-for x in range(1, 10):
-      files[x] = fileRoot + str(x)
+def solutionBias_ByComplexity(dataSet, solutionSet):
+      avgEstimators = {}
+      # for s in range(1, polynomialDegrees.__len__()+1):
+      sumEst = 0
+      for i in range(polynomialDegrees.__len__()):
+            sumEst = sumEst + solutionSet[i][2]
+      avgEstimators[s] = sumEst/numberOfTrainingSets
+      return avgEstimators
 
 dataSets = {}
-for x in range(1, 10):
-      dataSets[x] = {}
-      dataSets[x][1] = np.array(pd.read_csv(files[x], sep=',').values[:,0])
-      dataSets[x][2] = np.array(pd.read_csv(files[x], sep=',').values[:,1])
+for i in range(numberOfTrainingSets):
+      dataSets[i] = {}
+      x, y = createSineData(dataPointsPerTrainingSet, seedMap[i])
+      dataSets[i]['x'] = x
+      dataSets[i]['y'] = y
 
+#Train solutions for each training data set across several degrees of polynomial complexity
 solutions = {}
-for x in range(1, 10):
+for x in range(numberOfTrainingSets):
       solutions[x]={}
-      for s in range(0, polynomialDegrees.__len__()):
-            x1 = dataSets[x][1]
-            y1 = dataSets[x][2]
+      for s in range(polynomialDegrees.__len__()):
+            x1 = dataSets[x]['x']
+            y1 = dataSets[x]['y']
             func = np.polyfit(x1, y1, polynomialDegrees[s])
-            solutions[x][s+1] = poly1d(func)
+            solutions[x][s] = poly1d(func)
 
 xpoints = np.linspace(-math.pi, math.pi, 500)
 axes = plt.gca()
+axes.set_xlim([xmin,xmax])
 axes.set_ylim([ymin,ymax])
 
-index = 9
-x= dataSets[index][1]
-y = dataSets[index][2]
+index = 1
+solutionIndex = 0
+x = dataSets[index]['x']
+y = dataSets[index]['y']
 
-#******Plot Scatter Plot Of Training Data Set******
+
+# ******Plot Scatter Plot Of Training Data Set******
 # plt.plot(x,y,'bx')
 
 #******Plot By Polynomial Complexity For One Training Set******
-# for s in range(0, polynomialDegrees.__len__()):
-#       plt.plot(xpoints,solutions[index][s+1](xpoints),'r-')
+# for s in range(polynomialDegrees.__len__()):
+#       plt.plot(xpoints,solutions[index][s](xpoints),'r-')
 #       print("Mean squared error: %.6f"
-#             % np.mean((np.polyval(solutions[index][s+1], x) - y) ** 2))
+#             % np.mean((np.polyval(solutions[index][s], x) - y) ** 2))
 #
-      # print(solutions[index][s+1])
+#       print(solutions[index][s])
 # plt.show()
 
-#******Plot By Polynomial Complexity Across All Training Sets******
-# for x in range(1, 10):
+# ******Plot By Polynomial Complexity Across All Training Sets******
+# for i in range(numberOfTrainingSets):
 #       print('')
-#       plt.plot(xpoints,solutions[x][solutionIndex](xpoints),'r-')
+#       plt.plot(xpoints,solutions[i][solutionIndex](xpoints),'r-')
 #       plt.plot(xpoints, sin(xpoints), 'b-')
 #       print("Mean squared error: %.2f"
-#             % np.mean((np.polyval(solutions[index][s+1], x) - y) ** 2))
-#       print(solutions[index][s+1])
+#             % np.mean((np.polyval(solutions[index][s], x) - y) ** 2))
+#       print(solutions[index][s])
 # plt.show()
 
-
+# ******Training MSE by complexity for a training data set******
 # sB = training_MSE_ByComplexity(dataSets, solutions)
-# for sb in range(1, polynomialDegrees.__len__()+1) :
-#       print(sB[index][sb])
+# for s in range(polynomialDegrees.__len__()) :
+#       print(sB[index][s])
 
+
+# !!!!!!PENDING SPLIT OF DATA INTO TEST AND TRAIN!!!!!!
 # sB = solutionBias_ByComplexity(dataSets, solutions)
 # for bias in range(1, polynomialDegrees.__len__()+1) :
 #       print("%.32f"
 #             %sB[index][bias])
-
-
-x = dataSets[index][1]
-y = dataSets[index][2]
-poly = 1
-vals = []
-for e in range(0, 20):
-      val = np.polyval(solutions[index][poly], x[e])-y[e]
-      print('%0.32f'
-            %val)
-      vals.append(val)
-
-print("*************************")
-print(sum(vals))
-print("*************************")
-print('%0.32f'
-      %np.mean(np.polyval(solutions[index][poly], x) - y) )
 
 
 
